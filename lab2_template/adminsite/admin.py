32fe332b0591b9e6828cd8d6fc4b0eb9dc4f088c
shayleydon <shayleydon>
2025-07-29 12:39:14 +0100

    c9m3 django admin app, create Inline classes for adding related objects on the same admin pages

--- lab2_template/adminsite/admin.py
+++ lab2_template/adminsite/admin.py
@@ -3,8 +3,13 @@ from .models import Course, Instructor, Lesson
 
 # Register your models here.
 
+class LessonInline(admin.StackedInline):
+    model = Lesson 
+    extra = 5
+
 class CourseAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
     
 class InstructorAdmin(admin.ModelAdmin):
     fields = ['user', 'full_time']
