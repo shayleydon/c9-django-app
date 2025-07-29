1587e6c18ad67322438ba1e835b3370121c343b0
shayleydon <shayleydon>
2025-07-29 12:35:49 +0100

    c9m3 django admin app, customize model fields for admin pages

--- lab2_template/adminsite/admin.py
+++ lab2_template/adminsite/admin.py
@@ -3,5 +3,14 @@ from .models import Course, Instructor, Lesson
 
 # Register your models here.
 
-admin.site.register(Course)
-admin.site.register(Instructor)
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time']
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Instructor, InstructorAdmin)    
+
+
+
