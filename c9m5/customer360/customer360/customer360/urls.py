07488bd7a0e779edddba4add52d989113d25d620
shayleydon <shayleydon>
2025-07-30 13:36:32 +0100

    c9m5 customer360 completed

--- c9m5/customer360/customer360/customer360/urls.py
+++ c9m5/customer360/customer360/customer360/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+    path('create/',views.create_customer,name='create_customer'),
+    path('interact/<int:cid>',views.interact,name='interact'),
+    path('summary/',views.summary,name='summary'),
 ]
